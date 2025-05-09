f0d843a817e9550acbc42e7cb5413e23e7796f53
misantropy <biterolf10@gmail.com>
2025-05-09 16:02:37 +0500

    client commands and comments to files

--- client.py
+++ client.py
@@ -1,66 +1,96 @@
+# imports
 import socket
 import threading
 import sys
 import os
 
+# functions
+# convert string to int
 def str_to_int(str):
     try:
         return int(str)
     except (ValueError, TypeError):
         return None
 
-class Client: 
+# client class
+class Client:
+    # collect client info and what server he want
     def __init__(self):
         print("Welcome, please enter some information before chatting")
         
+        # get server address to connect
         self.address = None
         while not self.address:
             self.address = input("Type server address: ")
         
+        # get server port
         self.port = None
         while not self.port:
             self.port = str_to_int(input("Type server port: "))
-            
+        
+        # get optional username
         self.username = input("Type your username (optional): ")
         if not self.username:
             self.username = self.address
-            
+    
+    # "connect" to server (setting up socket to future communications)
     def connect(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.setblocking(False)
-        
+    
+    # get any new messages without a big delay
     def receive(self):
         while True:
+            # 1 seconds timeout to message get
             self.socket.settimeout(1)
             
             try:
+                # get new message if possible
                 resp, srvr = self.socket.recvfrom(1024)
+                
+                # beatifull message display and cursor position
                 sys.stdout.write(f"\033[s\033[1E{resp.decode()}\033[1L\033[1F\033[u")
                 sys.stdout.flush()
             except socket.timeout:
                 pass
-        
+    
+    # send message to server
     def message(self):
+        # setup cursor
         sys.stdout.write("\033[1A\033[K> ")
         sys.stdout.flush()
+        
+        # get message
         message = input()
         
-        if message.lower() != "/pass":
+        # commands
+        if message.lower() == "/dconnect":
+            # dissconect
+            self.close()
+        elif message.lower() == "/announce":
+            # make sound on message
+            message = message + "\a"
+        else: # default message
             data = f"{self.username}: {message}"
             self.socket.sendto(data.encode(), (self.address, self.port))
-        
+    
+    # close connection
     def close(self):
         self.socket.close()
-        
+
+# client and terminal setup
 def main():
+    # setup client
     client = Client()
     client.connect()
     
+    # setup terminal
     os.system("")
     os.system('cls' if os.name == 'nt' else 'clear')
     
+    # start thread for receiving messages
     threading.Thread(target=client.receive).start()
     
+    # loop for messaging
     while True:
         client.message()
 
--- server.py
+++ server.py
@@ -1,25 +1,34 @@
+# imports
 import socket
 
+# functions
+# convert string to int
 def str_to_int(str):
     try:
         return int(str)
     except (ValueError, TypeError):
         return None
 
+# server class
 class Server:
+    # get info about server
     def __init__(self):
         print("Welcome, please enter information to initialize server")
         
+        # get ip address
         self.address = None
         while not self.address:
             self.address = input("Type server address: ")
         
+        # get port (can be any)
         self.port = None
         while not self.port:
             self.port = str_to_int(input("Type server port: "))
-            
+        
+        # connected ips
         self.connected = []
     
+    # host server
     def host(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.address, self.port))
@@ -27,29 +36,37 @@ class Server:
         
         print(f"Server initizialized on {self.address}:{self.port}")
         
+    # activate to forward messages to all connected ips
     def activate(self):
         self.active = True
+        
         while self.active:
             try:
+                # attempt to get send data
                 data, sndr = self.socket.recvfrom(1024)
                 print(f"({sndr[0]}) {data.decode()}")
                 
+                # check if sender in ip list
                 try:
                     self.connected.index(sndr)
                 except ValueError:
                     self.connected.append(sndr)
 
+                # forward new message to every ip
                 for ip in self.connected:
                     self.socket.sendto(data, ip)
             except BlockingIOError:
                 pass
-            
+    
+    # deactivate message forwarding
     def deactivate(self):
         self.active = False
-        
+    
+    # close server
     def close(self):
         self.socket.close()
             
+# server initilization
 def main():
     server = Server()
     server.host()
